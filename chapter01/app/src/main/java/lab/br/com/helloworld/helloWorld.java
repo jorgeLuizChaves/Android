95033e43204ba343118e36de67e7dde4541834de
Jorge Oliveira <jorge.luizchaves@yahoo.com.br>
2014-12-31 19:43:03 -0200

    improves in the hello world app
    improves in the hello world app.

--- chapter01/app/src/main/java/lab/br/com/helloworld/helloWorld.java
+++ chapter01/app/src/main/java/lab/br/com/helloworld/helloWorld.java
@@ -2,16 +2,39 @@ package lab.br.com.helloworld;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class helloWorld extends ActionBarActivity {
+public class HelloWorld extends ActionBarActivity {
+
+    private EditText txtName;
+    private Button btnGreeting;
+    private TextView txtGreeting;
+
+    private static final String WHITE_SPACE = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        this.txtName = (EditText) findViewById(R.id.txt_name);
+        this.btnGreeting = (Button) findViewById(R.id.btn_greeting);
+        this.txtGreeting = (TextView) findViewById(R.id.lbl_greeting);
+
+        this.btnGreeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Editable text = HelloWorld.this.txtName.getText();
+                String greeting = getResources().getString(R.string.greeting);
+                HelloWorld.this.txtGreeting.setText(greeting + HelloWorld.WHITE_SPACE + text);
+            }
+        });
     }
 
 
